18fede8ff8bd339f8450b4e8e853465496dacc92
kawtarZineb_benkablia <hk_benkablia@esi.dz>
2021-07-01 10:42:08 +0100

    versionfinale3

--- Projet2CS/settings.py
+++ Projet2CS/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yv^x%)21*vyyg(bmv%wi24(-wouz^h_su*59g!q@%h+o#kn!3u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
